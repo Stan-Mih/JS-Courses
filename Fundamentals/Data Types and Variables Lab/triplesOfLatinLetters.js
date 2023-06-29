4d84f75e3d1f4ebb8a041a83d1105c69dc41d2f5
Stnislav Mihaylov <109460661+Stan-Mih@users.noreply.github.com>
2023-06-29 15:58:39 +0300

    exercise

--- Fundamentals/Data Types and Variables Lab/triplesOfLatinLetters.js
+++ Fundamentals/Data Types and Variables Lab/triplesOfLatinLetters.js
@@ -1,3 +1,18 @@
-function triplesOfLatinLetters() {
+function triplesOfLatinLetters(n) {
 
-}triplesOfLatinLetters()
+    for(let i = 0; i < n; i ++){
+        letter1 = String.fromCharCode(97+i);
+        for(let u = 0; u < n; u++){
+            letter2 = String.fromCharCode(97+u);
+            for(let y = 0; y < n; y++){
+                letter3 = String.fromCharCode(97+y);
+                console.log(letter1+letter2+letter3)
+            }
+        }
+
+        
+
+        ;
+    }
+}triplesOfLatinLetters(3)
+triplesOfLatinLetters(2)
