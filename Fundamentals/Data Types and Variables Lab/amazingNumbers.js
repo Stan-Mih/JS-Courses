c7f22bfe52cb3fffe601429241438531b4a28d59
Stnislav Mihaylov <109460661+Stan-Mih@users.noreply.github.com>
2023-06-29 13:42:40 +0300

    code uppgrade

--- Fundamentals/Data Types and Variables Lab/amazingNumbers.js
+++ Fundamentals/Data Types and Variables Lab/amazingNumbers.js
@@ -1,4 +1,5 @@
 function amazingNumbers(num) {
+    
     let string = num.toString()
     let sum = 0;
     for(i = 0; i < string.length; i++){
@@ -6,4 +7,6 @@ function amazingNumbers(num) {
     }
     let result = sum.toString().includes("9");
     result ? console.log(`${num} Amazing? True`) : console.log(`${num} Amazing? False`)
+
 }amazingNumbers(1233)
+amazingNumbers(999)
--- Fundamentals/Data Types and Variables Lab/specialNumbers.js
+++ Fundamentals/Data Types and Variables Lab/specialNumbers.js
@@ -8,9 +8,9 @@ function specialNumbers(num){
         for(i = 1; i <= length; i++){
         let lastNum = u % 10;
         sum += lastNum;
-        let remLastNum = parseInt(u / 10);
         }
-        if(sum === 5 || sum === 7 || sum ===11){
+        
+        if(sum === 5 || sum === 7 || sum === 11){
             console.log(`${u} -> True`);
         }else{console.log(`${u} -> False`);}
 
