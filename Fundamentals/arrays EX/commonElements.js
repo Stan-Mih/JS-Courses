3be8b18af25f00c3bb06119171cdf8bf120e1767
Stnislav Mihaylov <109460661+Stan-Mih@users.noreply.github.com>
2023-06-30 16:37:47 +0300

    exercise

--- Fundamentals/arrays EX/commonElements.js
+++ Fundamentals/arrays EX/commonElements.js
@@ -8,11 +8,18 @@ function commonElements(arr1, arr2) {
     //     }
     // }
 
-        for(i of arr1){
-            if(arr2.includes(i)){
-                console.log(i);
-            }
+    // for(i of arr1){
+    //     if(arr2.includes(i)){
+    //         console.log(i);
+    //     }
+    // }
+
+    for (const i of arr1) {
+        arr2.includes(i) ? console.log(i):console.log();
         }
+
 }commonElements(['Hey', 'hello', 2, 4, 'Peter', 'e'],
 ['Petar', 10, 'hey', 4, 'hello', '2']
 )
+commonElements(['S', 'o', 'f', 't', 'U', 'n', 'i', ' '],
+['s', 'o', 'c', 'i', 'a', 'l'])
