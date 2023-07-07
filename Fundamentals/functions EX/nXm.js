dcae9779259e9ee48d4c48421a099fda4cfe1fc9
Stnislav Mihaylov <109460661+Stan-Mih@users.noreply.github.com>
2023-07-07 09:27:03 +0300

    exercise

--- Fundamentals/functions EX/nXm.js
+++ Fundamentals/functions EX/nXm.js
@@ -1,16 +1,18 @@
 function nxn(num) {
- 
-    // for(let n = 1; n <= num; n++){
-    //     let toPrint = [];
+       
         
-    //     for(let m = 1; m <= num; m++){
-    //         toPrint.push(num);
-    //     }
-    //     console.log(toPrint.join(" "));
-    // }
-
-    
-
+       let rowGenerator = () => {
+        let toPrint = [];
+        for(let m = 1; m <= num; m++){
+            toPrint.push(num);
+        }
+        return toPrint.join(" ");
+       }
+        for(let n = 1; n <= num; n++){
+            console.log(rowGenerator());
+        }
+  
+  
 //     let matrix = [];
 //     for(let n = 0; n < num; n++){
 
@@ -25,8 +27,12 @@ function nxn(num) {
     
 
 
-    for(let i = 0; i < num; i++){
-        console.log(num.toString().repeat(num).split("").join(" "));
-    }
+    // for(let i = 0; i < num; i++){
+
+    //     console.log(num.toString().repeat(num).split("").join(" "));
+    // }
+
+
+
     
 }nxn(7) 
