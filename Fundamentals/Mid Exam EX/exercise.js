44d6836bab43d92ff235d6da7dfd8110aed48f5f
Stnislav Mihaylov <109460661+Stan-Mih@users.noreply.github.com>
2023-07-18 13:59:10 +0300

    exercise

---
+++ Fundamentals/Mid Exam EX/exercise.js
@@ -0,0 +1,22 @@
+function solve(array) {
+    
+    let arr = array.slice(0);   
+    let targetsArr = arr.shift().split(" ");
+    let comandsAndTarget = arr.shift();
+    
+    while (comandsAndTarget !== "End" ) {
+        
+        let command = comandsAndTarget. 
+    }
+}
+
+solve(["52 74 23 44 96 110",
+"Shoot 5 10",
+"Shoot 1 80",
+"Strike 2 1",
+"Add 22 3",
+"End"])
+
+solve(["1 2 3 4 5",
+"Strike 0 1",
+"End"])
