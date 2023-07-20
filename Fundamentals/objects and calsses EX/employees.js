ac6173b04bb6cf9fcc6ec4fbbf2a054c04bb195b
Stnislav Mihaylov <109460661+Stan-Mih@users.noreply.github.com>
2023-07-20 10:13:14 +0300

    task solution update

--- Fundamentals/objects and calsses EX/employees.js
+++ Fundamentals/objects and calsses EX/employees.js
@@ -1,31 +1,34 @@
 function employees(arr) {
+  let employees = {};
 
-    class Employee {
-        constructor(name, num) {
-            this.name = name;
-            this.num = num;
-        }
-        print(){
-            console.log(`Name: ${this.name} -- Personal Number: ${this.num}`);
-        }
-    }
-    arr.forEach(person => {
-        let currNameLen = person.length;
-        let currentEmployee = new Employee(person, currNameLen)
-        currentEmployee.print();
-    });    
+  arr.forEach((person) => {
+    employees[person] = person.length;
+  });
+  for (const key in employees) {
+    console.log(`Name: ${key} -- Personal Number: ${employees[key]}`);
+  }
+
+  // WITH CLASS
+  //
+  // class Employee {
+  //     constructor(name, num) {
+  //         this.name = name;
+  //         this.num = num;
+  //     }
+  //     print(){
+  //         console.log(`Name: ${this.name} -- Personal Number: ${this.num}`);
+  //     }
+  // }
+  // arr.forEach(person => {
+  //     let currNameLen = person.length;
+  //     let currentEmployee = new Employee(person, currNameLen)
+  //     currentEmployee.print();
+  // });
 }
 employees([
-    'Silas Butler',
-    'Adnaan Buckley',
-    'Juan Peterson',
-    'Brendan Villarreal'
-    ]
-    )
-employees([
-    'Samuel Jackson',
-    'Will Smith',
-    'Bruce Willis',
-    'Tom Holland'
-    ]
-    )
+  "Silas Butler",
+  "Adnaan Buckley",
+  "Juan Peterson",
+  "Brendan Villarreal",
+]);
+employees(["Samuel Jackson", "Will Smith", "Bruce Willis", "Tom Holland"]);
