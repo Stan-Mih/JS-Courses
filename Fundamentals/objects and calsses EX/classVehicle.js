b0b28b76a443b072df42f60b3d1ed82d87994cf3
Stnislav Mihaylov <109460661+Stan-Mih@users.noreply.github.com>
2023-07-22 10:39:19 +0300

    task update

--- Fundamentals/objects and calsses EX/classVehicle.js
+++ Fundamentals/objects and calsses EX/classVehicle.js
@@ -11,15 +11,13 @@ function classVehicle() {
         }
     }
 
-let parts = { engine: 6, power: 100 };
-let vehicle = new Vehicle('a', 'b', parts, 200);
-vehicle.drive(100);
-console.log(vehicle.fuel);
-console.log(vehicle.parts.quality);
-
-vehicle.array.forEach(element => {
+    let parts = {engine: 9, power: 500};
+    let vehicle = new Vehicle('l', 'k', parts, 840);
+    vehicle.drive(20);
+    console.log(vehicle.fuel);
     
-});
+
+
 
 }classVehicle()
 
