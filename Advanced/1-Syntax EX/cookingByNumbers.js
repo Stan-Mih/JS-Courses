2601f2c4b4070489d02fa92e8dfa731756b90827
Stnislav Mihaylov <109460661+Stan-Mih@users.noreply.github.com>
2023-08-14 14:04:39 +0300

    task update

--- Advanced/1-Syntax EX/cookingByNumbers.js
+++ Advanced/1-Syntax EX/cookingByNumbers.js
@@ -2,28 +2,40 @@ function cookingByNumbers(...imput) {
     
 let num = Number(imput.shift());
 
-imput.forEach(element => {
-    switch (element) {
-        case 'chop':
-            num /= 2;
-            break;
-        case 'dice':
-            num = Math.sqrt(num);
-            break;
-        case 'spice':
-            num ++;
-            break;
-        case 'bake':
-            num *= 3;
-            break;
-        case 'fillet':
-            num *= 0.8;
-            break;
+// imput.forEach(element => {
+//     switch (element) {
+//         case 'chop':
+//             num /= 2;
+//             break;
+//         case 'dice':
+//             num = Math.sqrt(num);
+//             break;
+//         case 'spice':
+//             num ++;
+//             break;
+//         case 'bake':
+//             num *= 3;
+//             break;
+//         case 'fillet':
+//             num *= 0.8;
+//             break;
     
-        default:
-            break;
-    }
-    console.log(num);
+//         default:
+//             break;
+//     }
+//     console.log(num);
+// });
+
+const functions = {
+    chop() {num /= 2},
+    dice() {num = Math.sqrt(num)},
+    spice() {num ++},
+    bake() {num *= 3},
+    fillet() {num *= 0.8}
+}
+
+imput.forEach(element => {functions[element](), console.log(num);
+
 });
 
 }
