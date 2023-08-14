0bf020b75fc13f43ecaa6c8ed0262d683e72acc7
Stnislav Mihaylov <109460661+Stan-Mih@users.noreply.github.com>
2023-08-14 12:00:49 +0300

    exercise

---
+++ Advanced/1-Syntax EX/timeToWalk.js
@@ -0,0 +1,15 @@
+function timeToWalk(steps, stepL, speed) {
+    const distanceInMeters = steps * stepL;
+    const breakTimeInSec = Math.floor(distanceInMeters / 500)* 60;
+    const speedInMS = (speed * 1000) / 3600;
+    const time = distanceInMeters / speedInMS + breakTimeInSec;
+
+    const hours = Math.floor(time / 3600);
+    const mins = Math.floor(time / 60);
+    const secs = Math.floor(time % 60) 
+
+    console.log(`${hours}:${mins}:${secs}`);
+   
+
+}timeToWalk(4000, 0.60, 5)
+timeToWalk(2564, 0.70, 5.5)
