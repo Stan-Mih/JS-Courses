2d48eb381d40a028db7edd539e9459606d3c5102
Stnislav Mihaylov <109460661+Stan-Mih@users.noreply.github.com>
2023-08-17 08:12:14 +0300

    exercise

---
+++ Advanced/2-2-ArraysAndNestedArrays EX/1-printAnArrayWithAGivenDelimiter.js
@@ -0,0 +1,18 @@
+function printAnArrayWithAGivenDelimiter(arr,divider) {
+    console.log(arr.join(divider));
+}
+printAnArrayWithAGivenDelimiter(['One', 
+'Two', 
+'Three', 
+'Four', 
+'Five'], 
+'-'
+)
+printAnArrayWithAGivenDelimiter(['How about no?', 
+'I',
+'will', 
+'not', 
+'do', 
+'it!'], 
+'_'
+)
