77af39eb7f828d2c6b6e454d9fb552fc2353a403
Stnislav Mihaylov <109460661+Stan-Mih@users.noreply.github.com>
2023-09-01 12:13:26 +0300

    update task

--- Advanced/4-1-Dom LAB/7-Sum_Table/SumTable.js
+++ Advanced/4-1-Dom LAB/7-Sum_Table/SumTable.js
@@ -2,7 +2,8 @@ function sumTable() {
     const list = document.querySelectorAll('table tr')
     let total = 0;
     for(let i = 1; i < list.length - 1;i++){    
-        const price = list[i].children[1].textContent;
+        const row = list[i].children;
+        const price = row[row.length-1].textContent;
         total+= Number(price)        
     }
     document.getElementById('sum').textContent = total
