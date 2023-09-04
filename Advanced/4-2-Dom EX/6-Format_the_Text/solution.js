e50c4fcc0b519187813bd3b1da6fd9f24e0aa1a9
Stnislav Mihaylov <109460661+Stan-Mih@users.noreply.github.com>
2023-09-04 13:07:23 +0300

    exercise

--- Advanced/4-2-Dom EX/6-Format_the_Text/solution.js
+++ Advanced/4-2-Dom EX/6-Format_the_Text/solution.js
@@ -1,3 +1,30 @@
 function solve() {
-  //TODO
+  const collection = document.querySelectorAll("p");
+  for (let i = 0; i < collection.length; i++) {
+    collection[i].remove();
+  }
+  const input = document.getElementById("input").value.split(".");
+  const out = document.getElementById("output");
+  let counter = 0;
+  let temp = [];
+
+  for (let i = 0; i < input.length; i++) {
+    temp.push(input[i]);
+    counter++;
+    if (counter === 3) {
+      const para = document.createElement("p");
+      const node = document.createTextNode(temp.join("."));
+      para.appendChild(node);
+      out.appendChild(para);
+      counter = 0;
+      temp = [];
+    } else if (counter < 3 && i === input.length - 1) {
+      const para = document.createElement("p");
+      const node = document.createTextNode(temp.join("."));
+      para.appendChild(node);
+      out.appendChild(para);
+      counter = 0;
+      temp = [];
+    }
+  }
 }
